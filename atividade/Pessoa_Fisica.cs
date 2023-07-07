4176f9f57788986224d715294af56f9e98c661de
willon franci <willonfranci@gmail.com>
2023-07-07 18:21:58 -0300

    atividade

---
+++ atividade/Pessoa_Fisica.cs
@@ -0,0 +1,12 @@
+namespace Atividade
+{
+    class Pessoa_Fisica : Clientes 
+    {
+        internal string endereco;
+
+        public string cpf {get; set;}
+        public string rg {get; set;}
+
+        
+    }
+}
